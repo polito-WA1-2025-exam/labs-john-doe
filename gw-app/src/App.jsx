275c55f712199c216db66ad1527b7659c7e059ee
babi00 <baby.frittella@gmail.com>
2025-04-04 11:09:50 +0200

    react

---
+++ gw-app/src/App.jsx
@@ -0,0 +1,19 @@
+import Header from './components/Header.jsx'
+import Footer from './components/Footer'
+
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+   <>
+    <Header/>
+    
+   </>
+  )
+}
+
+export default App;
--- gw-react-app/src/App.js
+++ gw-react-app/src/App.js
@@ -1,3 +1,6 @@
+import Header from '.components/Header'
+import Footer from '.components/Footer'
+
 import logo from './logo.svg';
 import './App.css';
 
