a4f8bd980410aeed0e1054a3d4a11355c33055ce
babi00 <baby.frittella@gmail.com>
2025-05-01 22:30:54 +0200

    updates

--- gw-app/src/components/MakeGuess.jsx
+++ gw-app/src/components/MakeGuess.jsx
@@ -27,25 +27,19 @@ function MakeGuess (props) {
       return (
         <Form onSubmit={handleSubmit} className="m-3">
           <Row>
-            <Col>
               <Form.Select value={feature} onChange={(e) => setFeature(e.target.value)}>
                 <option value="">Select feature</option>
                 {Object.keys(features).map((f, i) => (
                   <option key={i} value={f}>{f}</option>
                 ))}
               </Form.Select>
-            </Col>
-            <Col>
               <Form.Select value={value} onChange={(e) => setValue(e.target.value)} disabled={!feature}>
                 <option value="">Select value</option>
                 {feature && features[feature].map((v, i) => (
                   <option key={i} value={v}>{v}</option>
                 ))}
               </Form.Select>
-            </Col>
-            <Col>
               <Button type="submit">Guess</Button>
-            </Col>
           </Row>
         </Form>
       );
